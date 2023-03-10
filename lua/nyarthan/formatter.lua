b1685a727762a389313ecd227b44defd5e5f2b09
Jannis Morgenstern <nyarthan@proton.me>
2023-03-10 21:26:18 +0100

    feat: add typescript formatters

--- lua/nyarthan/formatter.lua
+++ lua/nyarthan/formatter.lua
@@ -1,5 +1,9 @@
 local M = {}
 
+M.__augroup = vim.api.nvim_create_augroup('Format', {})
+
+M.mason_dependencies = { 'stylua', 'prettierd' }
+
 M.setup = function()
   require('formatter').setup({
     logging = true,
@@ -8,16 +12,20 @@ M.setup = function()
       lua = {
         require('formatter.filetypes.lua').stylua,
       },
+      typescriptreact = {
+        require('formatter.filetypes.typescriptreact').prettierd,
+      },
+      typescript = {
+        require('formatter.filetypes.typescript').prettierd,
+      },
     },
   })
 end
 
-local augroup = vim.api.nvim_create_augroup('Formatting', {})
-
 M.on_attach = function(bufnr)
-  vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+  vim.api.nvim_clear_autocmds({ group = M.__augroup, buffer = bufnr })
   vim.api.nvim_create_autocmd('BufWritePost', {
-    group = augroup,
+    group = M.__augroup,
     buffer = bufnr,
     callback = U.make_cmd('FormatWriteLock'),
   })

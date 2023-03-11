e193a74e434b65d417ae42baa55dd65ea476867b
Jannis Morgenstern <nyarthan@proton.me>
2023-03-11 21:18:35 +0100

    feat: custom lsp signs

--- lua/nyarthan/lsp/init.lua
+++ lua/nyarthan/lsp/init.lua
@@ -3,9 +3,37 @@ local M = {}
 M.setup = function()
   local lsp_config = require('lspconfig')
   local capabilities = require('cmp_nvim_lsp').default_capabilities()
+  local icons = require('nyarthan.icons')
+
+  local signs = {
+    {
+      name = 'DiagnosticSignError',
+      text = icons.diagnostics.error,
+    },
+    {
+      name = 'DiagnosticSignWarn',
+      text = icons.diagnostics.warning,
+    },
+    {
+      name = 'DiagnosticSignHint',
+      text = icons.diagnostics.hint,
+    },
+    {
+      name = 'DiagnosticSignInfo',
+      text = icons.diagnostics.info,
+    },
+  }
+
   ---@diagnostic disable-next-line: unused-local
   local function on_attach(client, bufnr)
     require('nyarthan.formatter').on_attach(bufnr)
+
+    for _, sign in ipairs(signs) do
+      vim.fn.sign_define(
+        sign.name,
+        { texthl = sign.name, text = sign.text, numhl = '' }
+      )
+    end
 
     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 

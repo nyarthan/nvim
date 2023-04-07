3db922e5c3a749a71e08d74a8d948b07998a3472
Jannis Morgenstern <nyarthan@proton.me>
2023-04-07 22:21:18 +0200

    feat: add astro lsp support

--- lua/nyarthan/lsp/init.lua
+++ lua/nyarthan/lsp/init.lua
@@ -66,6 +66,11 @@ M.setup = function()
     on_attach = on_attach,
     capabilities = capabilities,
   })
+
+  lsp_config['astro'].setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+  })
 end
 
 return M
